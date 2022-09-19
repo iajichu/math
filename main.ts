eb6036c9d3a37b721cfa1e8b8b2f4ad8d14bba5a
iajichu <101339747+iajichu@users.noreply.github.com>
2022-09-19 13:53:53 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    sum = num + 5
+    basic.showNumber(sum)
+    product = 0
+    power = 0
+    random = 0
+})
+let random = 0
+let power = 0
+let product = 0
+let sum = 0
+let num = 0
+num = 5
+basic.forever(function () {
+	
+})
